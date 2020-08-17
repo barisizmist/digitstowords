603201f334d3f223b69ef6c0cfc2c6891cd58ed7
Mert Ocak <mertocak@gmail.com>
2020-08-17 22:16:47 +0300

    digits control

--- assets/js/towords.js
+++ assets/js/towords.js
@@ -90,9 +90,21 @@ function generateWords(number) {
 //Rakam kontrolü
 var input = document.getElementById("input");
 input.onkeypress = function (evt) {
-    var charCode = (evt.which) ? evt.which : event.keyCode
-    if (charCode > 31 && (charCode < 48 || charCode > 57))
-        return false;
+    var theEvent = evt || window.event;
+
+    // Handle paste
+    if (theEvent.type === 'paste') {
+        key = event.clipboardData.getData('text/plain');
+    } else {
+        // Handle key press
+        var key = theEvent.keyCode || theEvent.which;
+        key = String.fromCharCode(key);
+    }
+    var regex = /[0-9]|\./;
+    if (!regex.test(key)) {
+        theEvent.returnValue = false;
+        if (theEvent.preventDefault) theEvent.preventDefault();
+    }
 }
 
 //Butona tıklama sonrası
